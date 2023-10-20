1d02436bf9e666dd2a83d51f0dcdbaeb9e4ddfb7
vikartanSood <soodvikartan@gmail.com>
2023-10-21 01:32:42 +0530

    code complete

--- CurrencyConvertor/src/hooks/useCurrencyInfo.js
+++ CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -10,7 +10,7 @@ function userCurrencyInfo(currency){
           .then((res) => setData(res[currency]))
 
     } , [currency])
-
+//Here useEffect will take a call back i.e. arrow function and dependency array which is 'currency' here. Dependency array signifies that we will call API whenever there is change in any element of array.
     return data
 
 }
